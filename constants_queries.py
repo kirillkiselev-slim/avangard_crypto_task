4e95f1d44b414cb9240776cc247db4af81e95cd3
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-08-20 01:31:35 +0300

    finish tg handler func

--- constants_queries.py
+++ constants_queries.py
@@ -3,9 +3,12 @@ CREATE_CRYPTO_TABLE = ('CREATE TABLE IF NOT EXISTS'
 INSERT_CRYPTO_SLUG_NAMES = 'INSERT INTO crypto(crypto_slug) VALUES (?)'
 
 CREATE_USER_TABLE = ('CREATE TABLE IF NOT EXISTS user('
-                     'user TEXT UNIQUE, '
+                     'user TEXT, '
                      'crypto TEXT, '
                      'max_value REAL, '
                      'min_value REAL);')
 
 CHECK_CRYPTO_EXISTENCE = 'SELECT crypto_slug FROM crypto WHERE crypto_slug = ?'
+
+INSERT_USER_CRYPTO = ('INSERT INTO user(user, crypto, max_value, min_value)'
+                      ' VALUES(?, ?, ?, ?);')
