84f5fc8039d4caa5c4d0bee556284e6735ac6476
kirill_user <kiselev99kirill1501@gmail.com>
2024-08-19 22:31:22 +0300

    add all contstants

---
+++ constants_coinmarket.py
@@ -0,0 +1,19 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+LISTING_ENDPOINT_ALL_CRYPTOS = ('https://pro-api.coinmarketcap.com/v1/'
+                                'cryptocurrency/listings/latest')
+# LISTING_ENDPOINT_ALL_CRYPTOS = 'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
+
+HEADERS_COINMARKET = {
+    'Accepts': 'application/json',
+    'X-CMC_PRO_API_KEY': os.getenv('COINMARKET_API_KEY'),
+}
+
+PARAMS_COINMARKET = {
+    'start': 1,
+    'limit': 5000,
+}
