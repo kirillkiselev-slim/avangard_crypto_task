efbd8cd8f31b26846ef3343fb7f2f5c685ea1b50
kirillkiselev-slim <kiselev99kirill1501@gmail.com>
2024-08-21 19:55:08 +0300

    add class that controls the same username instance

---
+++ shared_state.py
@@ -0,0 +1,19 @@
+
+class Username:
+    _instance = None
+    username = None
+
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super(Username, cls).__new__(cls)
+            cls._instance.username = None
+        return cls._instance
+
+    def set_username(self, username):
+        self.username = username
+
+    def get_username(self):
+        return self.username
+
+
+username_instance = Username()
